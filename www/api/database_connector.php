ac00544ddd19a1cdb3a07b523f31c44094f354f8
Luca Bellon <luca.bellon@icloud.com>
2025-05-07 16:15:58 +0200

    set cred

--- www/api/database_connector.php
+++ www/api/database_connector.php
@@ -3,9 +3,9 @@
 function getDatabaseConnection() {
     // to use, set a variable to the return value of this function.
     $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "myDB";
+    $username = "root";
+    $password = "";
+    $dbname = "socialcreddit";
     
     // Create connection
     return new mysqli($servername, $username, $password, $dbname);
