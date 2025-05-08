21451884d121d9d24a3acca63ad7cacc5fb2dced
Luca Bellon <luca.bellon@icloud.com>
2025-05-08 22:22:45 +0200

    test lazyvim sry jungs

--- www/include/database/content_managment.php
+++ www/include/database/content_managment.php
@@ -1,15 +1,16 @@
 <?php
 require "database_connector.php";
 // To be used for creating posts and comments, and to get posts and comments from the database. 
-function get_posts($topic_id, $limit = 10, $sort_by = "created_at", $sort_order = "DESC") {
+function get_posts($topic_id, $limit = 10, $sort_by = "created_at", $sort_order = "DESC")
+{
     $conn = getDatabaseConnection();
-    $sql = 'SELECT * FROM posts WHERE TopicID = "'.$topic_id.'" ORDER BY "'.$sort_by.'" "'.$sort_order.'"LIMIT '.$limit;
+    $sql = 'SELECT * FROM posts WHERE TopicID = "' . $topic_id . '" ORDER BY "' . $sort_by . '" "' . $sort_order . '"LIMIT ' . $limit;
     $result = $conn->query($sql);
     $conn->close(); // Close the database connection
 
     if ($result->num_rows > 0) {
         $posts = array();
-        while($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
             $posts[] = $row;
         }
         return $posts;
@@ -18,24 +19,25 @@ function get_posts($topic_id, $limit = 10, $sort_by = "created_at", $sort_order 
     }
     // rückgabe der function sind die posts in einem array wie diesem: [0] => Array ( [ID] => 1 [TopicID] => 8 [Name] => #Willkommen im Forum der CCP. Schreibt rein worauf ihr bock habt aber bleibt im topic. Meinungsfreiheit wird hier *sehr* wertgeschätzt. [Likes] => 0 [Pinned] => 1 [UserID] => 3 )
     //um zum beispiel den name zu wollen muss mann nur $posts[0]['Name'] aufrufen fur den ersten post, alle einfach foreach schleife
-    
+
 }
 
-function get_topics($limit = 10, $sort_by = "popular") {
+function get_topics($limit = 10, $sort_by = "popular")
+{
     $conn = getDatabaseConnection();
     // select topics with the most posts
     if ($sort_by == "popular") {
-        $sql = 'SELECT * FROM topic ORDER BY (SELECT COUNT(*) FROM posts WHERE posts.TopicID = topic.ID) DESC LIMIT '.$limit;
+        $sql = 'SELECT * FROM topic ORDER BY (SELECT COUNT(*) FROM posts WHERE posts.TopicID = topic.ID) DESC LIMIT ' . $limit;
     } else {
         // select topics with the most likes
-        $sql = 'SELECT * FROM topic ORDER BY ID DESC LIMIT '.$limit;
+        $sql = 'SELECT * FROM topic ORDER BY ID DESC LIMIT ' . $limit;
     }
     $result = $conn->query($sql);
     $conn->close(); // Close the database connection
 
     if ($result->num_rows > 0) {
         $topics = array();
-        while($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
             $topics[] = $row;
         }
         return $topics;
@@ -45,5 +47,7 @@ function get_topics($limit = 10, $sort_by = "popular") {
 
     // rückgabe Array ( [0] => Array ( [ID] => 8 [Name] => Announcements ) [1] => Array ( [ID] => 9 [Name] => Katzen ) [2] => Array ( [ID] => 1 [Name] => Essen ) [3] => Array ( [ID] => 2 [Name] => Videospiele ) [4] => Array ( [ID] => 3 [Name] => Fischen ) [5] => Array ( [ID] => 4 [Name] => Security ) [6] => Array ( [ID] => 5 [Name] => Random ) [7] => Array ( [ID] => 6 [Name] => Politik ) [8] => Array ( [ID] => 7 [Name] => Programmieren ) [9] => Array ( [ID] => 10 [Name] => Lustige Bilder ) )
 }
-
-?>
+function get_comments()
+{
+    return null;
+}
