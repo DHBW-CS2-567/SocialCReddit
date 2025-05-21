1eed0cc761125352d3ced728e0c5a31f8d9547f0
Luca Bellon <luca.bellon@icloud.com>
2025-05-21 16:31:18 +0200

    .

--- www/post.php
+++ www/post.php
@@ -15,7 +15,7 @@
         $post_comments = get_post_comments(post_id: $post_id); 
         echo '<div class="post-content">';
         echo '<h2>' . htmlspecialchars($post_content['Content']) . '</h2>'; // Display post content
-        $date = date_create($post_content['created_at']);
+        $date = date_create($post_content['DateCreated']);
         echo '<p class="post-meta">Posted by ' . htmlspecialchars($post_content['username']) . ' on ' . date_format($date, 'F j, Y, g:i a') . '</p>';
         echo '<p class="post-meta">Likes: ' . htmlspecialchars($post_content['Likes']) . '</p>';
         echo '<p class="post-meta">Date Created: ' . htmlspecialchars($post_content['DateCreated']) . '</p>';
