f1473be9a34fba906ef256657af9c9b1d189e784
IliHanSoLow <ilian-ode@gmx.de>
2025-05-21 16:30:57 +0200

    fixed post php

--- www/post.php
+++ www/post.php
@@ -11,11 +11,11 @@
     include "include/topicbar.php";
     $post_id = isset($_GET['id']) ? $_GET['id'] : null; // get post id from url or set it null if not there
     if ($post_id) {
-        $post_content = get_post_content(post_id: $post_id); 
-        $post_comments = get_post_comments(post_id: $post_id); 
+        $post_content = get_post_content(post_id: $post_id);
+        $post_comments = get_post_comments(post_id: $post_id);
         echo '<div class="post-content">';
         echo '<h2>' . htmlspecialchars($post_content['Content']) . '</h2>'; // Display post content
-        $date = date_create($post_content['created_at']);
+        $date = date_create($post_content['DateCreated']);
         echo '<p class="post-meta">Posted by ' . htmlspecialchars($post_content['username']) . ' on ' . date_format($date, 'F j, Y, g:i a') . '</p>';
         echo '<p class="post-meta">Likes: ' . htmlspecialchars($post_content['Likes']) . '</p>';
         echo '<p class="post-meta">Date Created: ' . htmlspecialchars($post_content['DateCreated']) . '</p>';
